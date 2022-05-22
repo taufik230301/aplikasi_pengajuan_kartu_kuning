c218cd1edc11dc0d5dc8284b7f373d6117fb50ca
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-22 10:44:00 +0700

    add cetak laporan

--- application/controllers/Cetak.php
+++ application/controllers/Cetak.php
@@ -4,10 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cetak extends CI_Controller {
 
 
-    public function kartu_kuning()
-	{
-        
-		
+    public function laporan_pdf(){
+
+        $data = array(
+            "dataku" => array(
+                "nama" => "Petani Kode",
+                "url" => "http://petanikode.com"
+            )
+        );
+    
+        $this->load->library('pdf');
+    
+        $this->pdf->setPaper('A4', 'potrait');
+        $this->pdf->filename = "kartu_kuning.pdf";
+        $this->pdf->load_view('kartu_kuning_pdf', $data);
+    
+    
     }
 
 }
